cbb1d4c75274379a80cae480017f5d7ef0d6a354
Happollyon <36013973+Happollyon@users.noreply.github.com>
2019-09-17 18:39:36 +0100

    < included canvas to plot the graph>

--- tweet_analytics/src/App.js
+++ tweet_analytics/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import $ from 'jquery'
+
 import Header from './components/Header'
 import Footer from "./components/Footer";
 import Search from "./components/Search";
@@ -9,9 +9,9 @@ import './App.css'
 function App()
 {
     return(
-        <div>
+        <div id='app'>
         <Header/>
-        <Search/>
+        <Search />
         <GraphGenerator/>
         <Footer/>
         </div>
--- tweet_analytics/src/components/GraphGenerator.js
+++ tweet_analytics/src/components/GraphGenerator.js
@@ -6,18 +6,24 @@ class GraphGenerator extends Component
 constructor ()
 {
     super()
-this.recover = this.recover.bind(this)
+
+    this.state=
+        {
+         tweets: []
+        }
+    this.recover = this.recover.bind(this)
+    this.plot= this.plot.bind(this)
 }
 recover()
 {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=100&screen_name=Happollyon"
+    const url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name=u2"
 fetch(proxyurl + url,
     {
     method: "GET",
-
         headers :
-            { 'Authorization':' Bearer AAAAAAAAAAAAAAAAAAAAABEmAAEAAAAAk%2B0QCzyK88Du%2F5kzJkvMoiHaoZ4%3DYicmiHcDfgq1r535ZGPR1XZTUcDOZuVyu8e4DPfFb0BWbaeZ8R'
+            {
+                'Authorization':' Bearer AAAAAAAAAAAAAAAAAAAAABEmAAEAAAAAk%2B0QCzyK88Du%2F5kzJkvMoiHaoZ4%3DYicmiHcDfgq1r535ZGPR1XZTUcDOZuVyu8e4DPfFb0BWbaeZ8R'
             ,'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
@@ -26,15 +32,47 @@ fetch(proxyurl + url,
     .then(response=>response.json())
     .then(response=>
     {
+        this.setState({tweets:response})
 
-        console.log(response)
     })
+
 }
+
+plot()
+{
+
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+     var width = c.width / this.state.tweets.length
+    var width2= width
+    var height  = c.height/ Math.max.apply(Math, this.state.tweets.map(function(o) { return o.favorite_count; }))
+    alert(height)
+    ctx.strokeStyle='solid #fff'
+    ctx.translate(0, c.height);
+    ctx.scale(1, -1);
+    ctx.moveTo(0,0);
+    this.state.tweets.map(tweet=>
+        {
+            ctx.lineTo(width,tweet.favorite_count*height)
+            width = width + width2
+        }
+    )
+
+    ctx.stroke();
+    ctx.fill()
+
+
+}
+
+
 
 render(){
 
-    return(<div onClick={this.recover}>
-            <canvas></canvas>
+    return(<div id='graph'>
+            <button onClick={this.plot}>click</button>
+            <canvas id='canvas'  onClick={this.recover}>
+
+            </canvas>
         </div>
     )
 }
