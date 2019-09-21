b6eb49fb45a260d8a7da10e6cac7721f345db8bc
Happollyon <36013973+Happollyon@users.noreply.github.com>
2019-09-21 23:30:15 +0100

    < finished web version>

--- tweet_analytics/src/App.js
+++ tweet_analytics/src/App.js
@@ -1,21 +1,122 @@
-import React from 'react'
+import React , {Component} from 'react'
 import ReactDOM from 'react'
 import Header from './components/Header'
 import Footer from "./components/Footer";
 import Search from "./components/Search";
+import Card from "./components/Card";
 import GraphGenerator from "./components/GraphGenerator";
 import './App.css'
-function App()
+import * as Chart from "chart.js";
+class App extends React.Component
 {
+    constructor()
+    {
+        super()
+        this.state=
+            {        tweets: [],
+                 value:''
+            }
+        this.recover = this.recover.bind(this)
+        this.plot= this.plot.bind(this)
+        this.handleChange=this.handleChange.bind(this)
+    }
+    handleChange(event)
+    {
+        this.setState({value:event.target.value})
+    }
+    recover()
+    {   let user = this.state.value
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=50&screen_name="+user
+        fetch(proxyurl + url,
+            {
+                method: "GET",
+                headers :
+                    {
+                        'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAAGteAAEAAAAAlBECXFS19ucbvpSIqafMdiJltHI%3DhyY8Cy2EPF9d0WQz8xO3QJj6WXTBJPlGGEoJH8o5fjs6wNBjZm'
+                        ,'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+
+            })
+            .then(response=>
+            { if(response.status == 200)
+            {
+                response.json().then((response)=>
+                {  this.setState({tweets:response})
+                }).then(response=> {
+
+                    this.plot()})
+            }else{
+                alert('user does not exist.')
+                return
+            }
+
+            })
+
+        //  this.plot(user)
+    }
+
+    plot(user)
+    { var values =this.state.tweets.map(tweet=>tweet.favorite_count)
+        var lable = this.state.tweets.map(tweet=>tweet.created_at)
+
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+
+
+
+        var chart =  new  Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: lable,
+                datasets: [{
+                    label: 'likes',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: values,
+
+                }]
+
+            },
+
+            // Configuration options go here
+            options: {mode:'point'}
+        });
+        console.log(ctx.data)
+    }
+
+
+
+
+    render() {
+    let card
+
+    if(this.state.tweets == '')
+    {
+        card= <Card/>
+
+    }else
+    {
+        card  = <GraphGenerator/>
+    }
     return(
         <div id='app'>
 
-        <Header/>
-        <Search />
-        <GraphGenerator/>
-        <Footer/>
+            <Header/>
+
+            <Search value={this.state.value} recover={this.recover} handleChange = {this.handleChange} />
+
+            {card}
+
+            <Footer/>
         </div>
     )
+}
+
 
 }
 
--- tweet_analytics/src/components/GraphGenerator.js
+++ tweet_analytics/src/components/GraphGenerator.js
@@ -1,75 +1,12 @@
 import React , {Component} from 'react'
 
-import * as Chart from 'chart.js'
+
 class GraphGenerator extends Component
 {
 constructor ()
 {
     super()
 
-    this.state=
-        {
-         tweets: []
-        }
-    this.recover = this.recover.bind(this)
-    this.plot= this.plot.bind(this)
-
-
-}
-recover()
-{
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=50&screen_name=luscas"
-fetch(proxyurl + url,
-    {
-    method: "GET",
-        headers :
-            {
-                'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAAGteAAEAAAAAlBECXFS19ucbvpSIqafMdiJltHI%3DhyY8Cy2EPF9d0WQz8xO3QJj6WXTBJPlGGEoJH8o5fjs6wNBjZm'
-                ,'Content-Type': 'application/json',
-                'Accept': 'application/json'
-        }
-
-    })
-    .then(response=>response.json())
-    .then(response=>
-    {
-        this.setState({tweets:response})
-        console.log(response)
-    })
-
-}
-
-plot()
-{ var values =this.state.tweets.map(tweet=>tweet.favorite_count)
-    var lable = this.state.tweets.map(tweet=>tweet.created_at)
-
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-
-
-
-        var chart =  new  Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            labels: lable,
-            datasets: [{
-                label: 'likes',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: values,
-
-            }]
-
-        },
-
-        // Configuration options go here
-        options: {mode:'point'}
-    });
-    console.log(ctx.data)
 }
 
 
@@ -77,9 +14,9 @@ plot()
 render(){
 
     return(<div id='graph'>
-            {this.state.tweets.map(tweet=>tweet.favorite_count+',')}
-            <button onClick={this.plot}>click</button>
-            <canvas id='canvas'  onClick={this.recover}>
+
+
+            <canvas id='canvas'>
 
             </canvas>
         </div>
