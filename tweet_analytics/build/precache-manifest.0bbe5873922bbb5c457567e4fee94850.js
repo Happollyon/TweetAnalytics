b262842ef9df8cb0ee5c14483cdd4f840140f9d6
Happollyon <36013973+Happollyon@users.noreply.github.com>
2019-09-26 13:19:26 +0100

    final project

---
+++ tweet_analytics/build/precache-manifest.0bbe5873922bbb5c457567e4fee94850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ad1173b03c9944c573ffb99042238f",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "8be02e0dcb2f4ac9eaba",
+    "url": "/twitter/static/css/main.f605d054.chunk.css"
+  },
+  {
+    "revision": "cbbcf2ee299c9da701d6",
+    "url": "/twitter/static/js/2.1cd17caa.chunk.js"
+  },
+  {
+    "revision": "8be02e0dcb2f4ac9eaba",
+    "url": "/twitter/static/js/main.01060b0b.chunk.js"
+  },
+  {
+    "revision": "717f0cb7c9a0860183a5",
+    "url": "/twitter/static/js/runtime~main.ab448c16.js"
+  }
+]);
